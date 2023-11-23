17d2fca40f63bbdd980070dba2229b18311b31fc
jral <jral.wdev@gmail.com>
2023-11-23 17:47:02 +0800

    modified rollback serverjs

--- server.js
+++ server.js
@@ -6,6 +6,4 @@ import app from "./app/app.js";
 // Create the server
 const PORT = process.env.PORT || 8000
 const server = http.createServer(app)
-const io = new WebSocket.Server({ noServer: true });
-global.io = new WebSocket.Server({ noServer: true });
-server.listen(PORT, console.log(`Server started on port: ${PORT}`))
+server.listen(PORT, console.log(`Server started on port: ${PORT}`));
