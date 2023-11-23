fbb59b5c85d1af98bddf493d08542557c5e62910
jral <jral.wdev@gmail.com>
2023-11-23 17:44:25 +0800

    test for render with const io

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ import app from "./app/app.js";
 // Create the server
 const PORT = process.env.PORT || 8000
 const server = http.createServer(app)
+const io = new WebSocket.Server({ noServer: true });
+global.io = new WebSocket.Server({ noServer: true });
 server.listen(PORT, console.log(`Server started on port: ${PORT}`))
